d576bc48f7527d104acc0083f5a3cd408a6d5c39
Robert Joosten <rwm.joosten@gmail.com>
2022-02-20 11:46:15 +0000

    test unittest error code

--- mayaunittest/__main__.py
+++ mayaunittest/__main__.py
@@ -1,15 +1,14 @@
 def main():
     import os
+    import atexit
     import unittest
-    import maya.utils
     import maya.standalone
 
     maya.standalone.initialize()
     program = unittest.main(module=None, exit=False)
-
-    maya.utils.processIdleEvents()
     maya.standalone.uninitialize()
 
+    atexit._run_exitfuncs()
     ret_code = not program.result.wasSuccessful()
     os._exit(ret_code)  # sys.exit default to 0 at times.
 
