8c846e17e7d71e845cc3e11bb0099fa6f04e2a28
Robert Joosten <rwm.joosten@gmail.com>
2022-02-20 11:43:14 +0000

    test unittest return code

--- mayaunittest/__main__.py
+++ mayaunittest/__main__.py
@@ -1,6 +1,5 @@
 def main():
-    import sys
-    import atexit
+    import os
     import unittest
     import maya.utils
     import maya.standalone
@@ -10,11 +9,9 @@ def main():
 
     maya.utils.processIdleEvents()
     maya.standalone.uninitialize()
-    atexit._run_exitfuncs()
 
     ret_code = not program.result.wasSuccessful()
-    sys.exit(ret_code)
-    #os._exit(ret_code)  # sys.exit default to 0 at times.
+    os._exit(ret_code)  # sys.exit default to 0 at times.
 
 
 if __name__ == "__main__":
